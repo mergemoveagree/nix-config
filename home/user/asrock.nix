8d589e105301af09f1aec46376470d64c03ffe49
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-14 18:08:02 -0600

    Add libreoffice feature

--- home/user/asrock.nix
+++ home/user/asrock.nix
@@ -6,6 +6,7 @@
     "home/common/core"
     "home/common/features/desktop"
     "home/common/features/gaming"
+    "home/common/features/office.nix"
   ]);
 
   home.packages = [ pkgs.nixvim-neovim ];

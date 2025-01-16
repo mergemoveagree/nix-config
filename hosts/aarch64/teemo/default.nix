209db8ebbcd000ae78ad2d339a9f576a5a93c182
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-15 19:56:08 -0600

    Add static IP to teemo host

--- hosts/aarch64/teemo/default.nix
+++ hosts/aarch64/teemo/default.nix
@@ -17,11 +17,24 @@
 
   hostSpec = {
     hostName = "teemo";
-    netInterface = "eth0";
+    netInterface = "eno0";
     enableZsh = true;
     isServer = true;
   };
 
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.initrd.systemd.enable = lib.mkForce false;
+
+  networking = {
+    interfaces.eno0 = {
+      ipv4.addresses = [{
+        address = "192.168.1.3";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = {
+      address = "192.168.1.254";
+      interface = "ens3";
+    };
+  };
 }

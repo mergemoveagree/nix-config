c7c3ae99f70b8d1f9afb6ee3910bed2f61960b49
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-15 20:55:31 -0600

    Set up SSH and add teemo access key for use

---
+++ home/user/global/ssh.nix
@@ -0,0 +1,18 @@
+{ config
+, ...
+}: {
+  sops.secrets."ssh_user_teemo_access_public_key".sopsFile = ../secrets.yml;
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "teemo" = {
+        hostname = "192.168.1.3";
+        identitiesOnly = true;
+        identityFile = config.sops.secrets."ssh_user_teemo_access_public_key".path;
+        user = "user";
+      };
+    };
+  };
+}
+

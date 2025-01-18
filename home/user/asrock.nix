0b29db98e551b80edb613eda7fe1c0454bbf08b4
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-18 10:05:27 +0000

    Add user local features for asrock

--- home/user/asrock.nix
+++ home/user/asrock.nix
@@ -1,16 +1,20 @@
 { lib
 , ...
 }: {
-  imports = (map lib.custom.relativeToRoot [
-    "home/common/core"
-    "home/common/features/desktop"
-    "home/common/features/gaming"
-    "home/common/features/media.nix"
-    "home/common/features/comms.nix"
-    "home/common/features/office.nix"
+  imports = lib.flatten [
+    (map lib.custom.relativeToRoot [
+      "home/common/core"
+      "home/common/features/desktop"
+      "home/common/features/gaming"
+      "home/common/features/media.nix"
+      "home/common/features/comms.nix"
+      "home/common/features/office.nix"
+    ])
 
-    "home/user/features/comms.nix"
-  ]);
+    ./global
+    ./features/comms.nix
+    ./features/thunderbird.nix
+  ];
 
   services.easyeffects.preset = "Perfect EQ";
 }

b55e28f413b0d1c31272a2f39136b9abf4002b97
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-18 09:13:37 +0000

    Add oh-my-posh to cli feature

--- home/common/core/cli.nix
+++ home/common/core/cli.nix
@@ -13,11 +13,18 @@
     };
   };
 
+  programs.oh-my-posh = {
+    enable = true;
+    enableZshIntegration = config.hostSpec.enableZsh;
+    enableBashIntegration = ! config.hostSpec.enableZsh;
+    useTheme = "kushal";
+  };
+
   programs.fzf.enable = true;
   programs.fzf.enableZshIntegration = config.hostSpec.enableZsh;
-  programs.fzf.enableBashIntegration = !config.hostSpec.enableZsh;
+  programs.fzf.enableBashIntegration = ! config.hostSpec.enableZsh;
 
   programs.thefuck.enable = true;
   programs.thefuck.enableZshIntegration = config.hostSpec.enableZsh;
-  programs.thefuck.enableBashIntegration = !config.hostSpec.enableZsh;
+  programs.thefuck.enableBashIntegration = ! config.hostSpec.enableZsh;
 }

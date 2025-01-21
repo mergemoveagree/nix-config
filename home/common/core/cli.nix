15d91cc21a8f9d67eca9613a1e99f7a2378a31f0
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-20 21:12:54 -0600

    Improve cli feature

--- home/common/core/cli.nix
+++ home/common/core/cli.nix
@@ -1,30 +1,239 @@
 { config
 , lib
+, pkgs
 , ...
 }: {
+  home.packages = [
+    pkgs.nerd-fonts.fira-mono
+  ];
+
   programs.zsh = lib.optionalAttrs config.hostSpec.enableZsh {
     enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -l";
-    };
+    plugins = [
+      {
+        # TODO: Any way to do this without constantly changing the hashes?
+        # WARN: Needs to be updated regularly
+        name = "zsh-smartcache";
+        src = pkgs.fetchFromGitHub {
+          owner = "QuarticCat";
+          repo = "zsh-smartcache";
+          rev = "641dbfa196c9f69264ad7a49f9ef180af75831be";
+          hash = "sha256-t6QbAMFJfCvEOtq1y/YXZz5eyyc5OHOM/xg3cgXNcjU=";
+        };
+      }
+    ];
   };
 
-  programs.oh-my-posh = {
+  programs.fzf = {
     enable = true;
     enableZshIntegration = config.hostSpec.enableZsh;
     enableBashIntegration = ! config.hostSpec.enableZsh;
-    useTheme = "kushal";
   };
 
-  programs.fzf.enable = true;
-  programs.fzf.enableZshIntegration = config.hostSpec.enableZsh;
-  programs.fzf.enableBashIntegration = ! config.hostSpec.enableZsh;
+  programs.thefuck = {
+    enable = true;
+    enableZshIntegration = config.hostSpec.enableZsh;
+    enableBashIntegration = ! config.hostSpec.enableZsh;
+  };
 
-  programs.thefuck.enable = true;
-  programs.thefuck.enableZshIntegration = config.hostSpec.enableZsh;
-  programs.thefuck.enableBashIntegration = ! config.hostSpec.enableZsh;
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = config.hostSpec.enableZsh;
+    enableBashIntegration = ! config.hostSpec.enableZsh;
+  };
+
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = config.hostSpec.enableZsh;
+    enableBashIntegration = ! config.hostSpec.enableZsh;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = config.hostSpec.enableZsh;
+    enableBashIntegration = ! config.hostSpec.enableZsh;
+  };
+
+  programs.carapace = {
+    enable = true;
+    enableZshIntegration = config.hostSpec.enableZsh;
+    enableBashIntegration = ! config.hostSpec.enableZsh;
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = config.hostSpec.enableZsh;
+    enableBashIntegration = ! config.hostSpec.enableZsh;
+    settings = {
+      # Copied the gruvbox-rainbow preset into nix
+      format = lib.concatStrings [
+        "[](color_orange)"
+        "$os"
+        "$username"
+        "[](bg:color_yellow fg:color_orange)"
+        "$directory"
+        "[](fg:color_yellow bg:color_aqua)"
+        "$git_branch"
+        "$git_status"
+        "[](fg:color_aqua bg:color_blue)"
+        "$c"
+        "$rust"
+        "$golang"
+        "$nodejs"
+        "$php"
+        "$java"
+        "$kotlin"
+        "$haskell"
+        "$python"
+        "[](fg:color_blue bg:color_bg3)"
+        "$docker_context"
+        "$conda"
+        "[](fg:color_bg3 bg:color_bg1)"
+        "$time"
+        "[ ](fg:color_bg1)"
+        "$line_break"
+        "$character"
+      ];
+
+      palette = "gruvbox_dark";
+      palettes.gruvbox_dark = {
+        color_fg0 = "#fbf1c7";
+        color_bg1 = "#3c3836";
+        color_bg3 = "#665c54";
+        color_blue = "#458588";
+        color_aqua = "#689d6a";
+        color_green = "#98971a";
+        color_orange = "#d65d0e";
+        color_purple = "#b16286";
+        color_red = "#cc241d";
+        color_yellow = "#d79921";
+      };
+
+      os = {
+        disabled = false;
+        style = "bg:color_orange fg:color_fg0";
+        symbols = {
+          NixOS = " ";
+        };
+      };
+
+      username = {
+        show_always = true;
+        style_user = "bg:color_orange fg:color_fg0";
+        style_root = "bg:color_orange fg:color_fg0";
+        format = "[ $user ]($style)";
+      };
+
+      directory = {
+        style = "fg:color_fg0 bg:color_yellow";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+        substitutions = {
+          "Documents" = "󰈙 ";
+          "Downloads" = " ";
+          "Music" = "󰝚 ";
+          "Pictures" = " ";
+          "Developer" = "󰲋 ";
+        };
+      };
+
+      git_branch = {
+        symbol = "󰘬 ";
+        style = "bg:color_aqua";
+        format = "[[ $symbol $branch ](fg:color_fg0 bg:color_aqua)]($style)";
+      };
+
+      git_status = {
+        style = "bg:color_aqua";
+        format = "[[($all_status$ahead_behind )](fg:color_fg0 bg:color_aqua)]($style)";
+      };
+
+      nodejs = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+
+      c = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+
+      rust = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+
+      golang = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+      php = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+
+      java = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+
+      kotlin = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+
+      haskell = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+
+      python = {
+        symbol = " ";
+        style = "bg:color_blue";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      };
+
+      docker_context = {
+        symbol = " ";
+        style = "bg:color_bg3";
+        format = "[[ $symbol( $context) ](fg:#83a598 bg:color_bg3)]($style)";
+      };
+
+      conda = {
+        style = "bg:color_bg3";
+        format = "[[ $symbol( $environment) ](fg:#83a598 bg:color_bg3)]($style)";
+      };
+
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = "bg:color_bg1";
+        format = "[[  $time ](fg:color_fg0 bg:color_bg1)]($style)";
+      };
+
+      line_break = {
+        disabled = false;
+      };
+
+      character = {
+        disabled = false;
+        success_symbol = "[](bold fg:color_green)";
+        error_symbol = "[](bold fg:color_red)";
+        vimcmd_symbol = "[](bold fg:color_green)";
+        vimcmd_replace_one_symbol = "[](bold fg:color_purple)";
+        vimcmd_replace_symbol = "[](bold fg:color_purple)";
+        vimcmd_visual_symbol = "[](bold fg:color_yellow)";
+      };
+    };
+  };
 }

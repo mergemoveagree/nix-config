c904add451a79ee576857c2978ce9ed5bb377fb1
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-23 04:16:43 -0600

    Use static IP for asrock

--- hosts/nixos/asrock/default.nix
+++ hosts/nixos/asrock/default.nix
@@ -53,6 +53,19 @@ in {
     };
   };
 
+  networking = {
+    interfaces.enp6s0 = {
+      ipv4.addresses = [{
+        address = "192.168.1.5";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = {
+      address = "192.168.1.254";
+      interface = "enp6s0";
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     clinfo
     lact

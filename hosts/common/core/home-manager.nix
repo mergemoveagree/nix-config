160c55696976ea19350fd58b1b5a1299314ad60f
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-23 20:18:13 -0600

    Add optional check for sops-nix

--- hosts/common/core/home-manager.nix
+++ hosts/common/core/home-manager.nix
@@ -1,10 +1,11 @@
 { inputs
 , pkgs
 , config
+, lib
 , ...
 }: {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
-  home-manager.sharedModules = [ inputs.sops-nix.homeManagerModules.sops ];
+  home-manager.sharedModules = lib.optional (inputs ? "sops-nix") inputs.sops-nix.homeManagerModules.sops;
   home-manager.extraSpecialArgs = {
     inherit
       inputs

5c6fdfef58c59d33f2cea51e5b39637935ac115e
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-24 21:52:29 -0600

    Fix teemo-unlock ssh user

--- home/user/global/ssh.nix
+++ home/user/global/ssh.nix
@@ -17,7 +17,7 @@
         hostname = "192.168.1.3";
         identitiesOnly = true;
         identityFile = config.sops.secrets."ssh_user_teemo_access_private_key".path;
-        user = "user";
+        user = "root";
         port = 2222;
       };
 

9944194e9995ae0f61ebc17fa14e54726a9ff8f2
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-01-26 22:36:55 -0600

    Change rofi theme

--- home/common/features/desktop/hyprland/rofi/default.nix
+++ home/common/features/desktop/hyprland/rofi/default.nix
@@ -1,8 +1,16 @@
 { pkgs
 , ...
-}: {
+}: let
+  rofi-themes-collection = pkgs.fetchFromGitHub {
+    owner = "newmanls";
+    repo = "rofi-themes-collection";
+    rev = "c2be059e9507785d42fc2077a4c3bc2533760939";
+    hash = "sha256-pHPhqbRFNhs1Se2x/EhVe8Ggegt7/r9UZRocHlIUZKY=";
+  };
+in {
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    theme = "${rofi-themes-collection}/themes/simple-tokyonight.rasi";
   };
 }

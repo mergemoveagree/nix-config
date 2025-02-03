2085be99fe104f97e8216a9b7d9263168afc173d
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-02-03 14:52:32 -0600

    Add sddm-astronaut theme to SDDM

--- hosts/common/features/hyprland/sddm.nix
+++ hosts/common/features/hyprland/sddm.nix
@@ -1,9 +1,37 @@
-{
+{ pkgs
+, ...
+}: {
+  environment.systemPackages = with pkgs; [
+    ((sddm-astronaut.override {
+      # Changing deafault SDDM theme
+      themeConfig = {
+        ConfigFile = "Themes/pixel_sakura.conf";
+      };
+    }).overrideAttrs ({ installPhase ? "", ... }: {
+      # Moving fonts to correct directory
+      installPhase = installPhase + ''
+        mkdir -p $out/share/fonts
+        cp -r $out/share/sddm/themes/sddm-astronaut-theme/Fonts/* $out/share/fonts/
+        sed -i 's?ConfigFile=.*?ConfigFile=Themes/pixel_sakura.conf?' $out/share/sddm/themes/sddm-astronaut-theme/metadata.desktop
+      '';
+    }))
+  ];
+
   services.displayManager = {
     defaultSession = "hyprland-uwsm";
     sddm = {
       enable = true;
       wayland.enable = true;
+      package = pkgs.kdePackages.sddm;
+      extraPackages = with pkgs.kdePackages; [
+        qtmultimedia
+      ];
+      theme = "sddm-astronaut-theme";
+      settings = {
+        General = {
+          InputMethod = "qtvirtualkeyboard";
+        };
+      };
     };
   };
 }

ffb6d077ab9c7accaaf2c1b459eca6a5e0d81f52
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-03-04 13:15:37 -0600

    Optimize settings for unbound

--- hosts/aarch64/teemo/unbound.nix
+++ hosts/aarch64/teemo/unbound.nix
@@ -18,6 +18,21 @@
         port = 5335;
         access-control = [ "127.0.0.1 allow" ];
         root-hints = "/etc/unbound/root.hints";
+
+        # Optimization
+        num-threads = 4;
+        msg-cache-slabs = 4;
+        rrset-cache-slabs = 4;
+        infra-cache-slabs = 4;
+        key-cache-slabs = 4;
+        msg-cache-size = "50m";
+        rrset-cache-size = "100m";
+        outgoing-range = 206;
+        num-queries-per-thread = 103;
+        so-rcvbuf = "4m";
+        so-sndbuf = "4m";
+        so-reuseport = true;
+
         # Based on recommended settings in https://docs.pi-hole.net/guides/dns/unbound/#configure-unbound
         harden-glue = true;
         harden-dnssec-stripped = true;
@@ -29,5 +44,13 @@
         hide-version = true;
       };
     };
+  };
+
+  # Increase kernel buffer for unbound
+  boot.kernel.sysctl = {
+    "net.core.rmem_max" = 4194304;
+    "net.core.wmem_max" = 4194304;
+    "net.core.somaxconn" = 256;
+    "net.ipv4.tcp_max_syn_backlog" = 256;
   };
 }

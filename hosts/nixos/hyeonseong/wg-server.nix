a8a1dc3f16cfde7a2401efa96a0b7aa8bccd75a4
Jaden Nola <180627245+mergemoveagree@users.noreply.github.com>
2025-03-21 15:07:38 -0500

    Do not open extra port

--- hosts/nixos/hyeonseong/wg-server.nix
+++ hosts/nixos/hyeonseong/wg-server.nix
@@ -15,11 +15,10 @@
     };
 
     firewall = {
-      allowedUDPPorts = [ 51820 2223 ];
+      allowedUDPPorts = [ 51820 ];
       extraCommands = ''
         ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.100.0.2:80
         ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination 10.100.0.2:443
-        ${pkgs.iptables}/bin/iptables -t nat -A PREROUTING -p tcp --dport 2223 -j DNAT --to-destination 10.100.0.2:2223
       '';
     };
 
